2bd5ae067f8649e07614a31ae08bb2c35a65c6db
Ananya Putatunda <93035646+Ananya-12-cpu@users.noreply.github.com>
2026-07-01 22:23:30 +0530

    Revert "Add files via upload"
    This reverts commit 74e865b327b7dcb079cc52824e39163565c52ecc.

--- Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-zinc-200 py-8 text-center text-sm text-zinc-500 dark:border-zinc-800 dark:text-zinc-400">
-      <p>&copy; {new Date().getFullYear()} Shoply. All rights reserved.</p>
-    </footer>
-  );
-}
